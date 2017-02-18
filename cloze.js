46b7ad1fdb98e80f31a10cdfbe17fd2f01fa5ad7
Matt White <mattwhite1337@gmail.com>
2017-02-18 17:21:00 -0500

    initial commit

---
+++ cloze.js
@@ -0,0 +1,25 @@
+
+var ClozeFlashcard = function(cloze, question){
+	this.cloze = cloze;
+	this.question = question;
+
+	this.displayQuestion = function(){
+		var display = question.replace(cloze, "...");
+		console.log(display);
+	}
+
+}
+
+var arr = [];
+
+var material = {
+	"1976": "The first Apple computer was released in 1976",
+	"2004": "Facebook was founded in 2004",
+	"Graphical User Interface": "GUI stands for Graphical User Interface"
+};
+
+for (var key in material){
+	arr.push(new ClozeFlashcard(key, material[key]));
+}
+
+console.log(arr[0].displayQuestion());
