06feb6937335bdcdb8e11623fa183222e2b33295
Matt White <mattwhite1337@gmail.com>
2017-02-19 11:20:11 -0500

    working flashcard program

--- cloze.js
+++ cloze.js
@@ -15,11 +15,14 @@ var arr = [];
 var material = {
 	"1976": "The first Apple computer was released in 1976",
 	"2004": "Facebook was founded in 2004",
-	"Graphical User Interface": "GUI stands for Graphical User Interface"
+	"Graphical User Interface": "GUI stands for Graphical User Interface",
+	"Structured Query Language": "SQL is an acronym for Structured Query Language",
+	"1990": "CERN launched the first website in the year 1990",
+	"Bill Gates": "Bill Gates, a Harvard dropout, co-founded Microsoft"
 };
 
 for (var key in material){
 	arr.push(new ClozeFlashcard(key, material[key]));
 }
 
-module.exports = arr;
+module.exports = {arr, ClozeFlashcard};
--- flashcards.js
+++ flashcards.js
@@ -1,33 +1,122 @@
-var arrOfFlashcards = require('./cloze.js');
+var clozeExports = require('./cloze.js');
 
 var inquirer = require('inquirer');
 
 var count = 0;
+var rightCount = 0;
+var wrongCount = 0;
+
+
+function start(){
+	console.log('');
+	console.log('==================================');
+	console.log('Welcome to the Flashcard Generator');
+	console.log('==================================');
+
+	inquirer.prompt([
+
+		{
+			type: "list",
+			message: "Make your choice",
+			choices: ["Quiz me!", "Add a flashcard", "End Program"],
+			name: "choices"
+		}
+
+	]).then(function(data){
+
+		if(data.choices === "Quiz me!"){
+			quiz();
+		}else if(data.choices === "Add a flashcard"){
+
+			inquirer.prompt([
+
+				{
+					type: "input",
+					message: "Enter the text that will be your answer",
+					name: "cloze",
+					default: ""
+				},
+
+				{
+					type: "input",
+					message: "Enter the entire question including your answer",
+					name: "question",
+					default: ""
+				}
+
+			]).then(function(data){
+				clozeExports.arr.push(new clozeExports.ClozeFlashcard(data.cloze, data.question));
+				start();
+			})
+
+		}else {
+			process.exit();
+		};
+
+	});
+};
 
 function quiz(){
-	if(count < arrOfFlashcards.length){
+	if(count < clozeExports.arr.length){
+
 		inquirer.prompt([
 
 			{
 				type: "input",
-				message: `${arrOfFlashcards[count].displayQuestion()}`,
+				message: `${clozeExports.arr[count].displayQuestion()}`,
 				name: "question",
 				default: ""
 			}
 
 		]).then(function(data){
 
-			if(data.question === arrOfFlashcards[count].cloze) {
+			if(data.question === clozeExports.arr[count].cloze) {
 				console.log('correct');
+				rightCount++;
 			}else {
-				console.log(arrOfFlashcards[count].question);
-			}
-			
+				console.log(clozeExports.arr[count].question);
+				wrongCount++;
+			};
+
 			count++;
 			quiz();
 
 		});
+
+	}else {
+		gameOver();
 	};
+
 };
 
-quiz();
+function gameOver(){
+	console.log(`You had ${rightCount} right answers and ${wrongCount} wrong answers`);
+	console.log("");
+
+	inquirer.prompt([
+
+		{
+			type: "confirm",
+			name: "confirm",
+			message: "Play again?"
+		}
+
+	]).then(function(data){
+
+		if(data.confirm){
+
+			count = 0;
+			rightCount = 0;
+			wrongCount = 0;
+
+			quiz();
+
+		}else {
+			start();
+		}
+
+	});
+
+};
+
+start();
